52ca0332e5421ff0c5b0c182c0b7ed64e7e99b64
portellam@gitkraken <name@example.com>
2022-09-30 00:22:05 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@
 # Author(s):    Alex Portell <github.com/portellam>
 #
 
-
 # check if sudo/root #
     function CheckIfUserIsRoot
     {
@@ -16,19 +15,17 @@
         fi
     }
 
-#
-    function CreateAndAppendBackupFromFile {
+# create backup #
+    function CreateBackupFromFile
+    {
 
         # behavior:
-        #   create latest backup from file (do not save first-time backup)
-        #   if file does not exist or is not readable, throw exception
-        #   else, continue
-        #   if backup exists, check if source file is newer, and create new backup with increment
-        #       else, do nothing
-        #   else, create backup
+        # create a backup file
+        # exception handling
+        # garbage collection: delete all but last n backup files
         #
 
-        # NOTE: nested if statements are *slow*, and confusing
+        echo -en "Backing up file... "
 
         # parameters #
         str_thisFile=$1
@@ -36,172 +33,197 @@
         # create false match statements before work begins, to catch exceptions
             # null exception
             if [[ -z $str_thisFile ]]; then
-                echo -e "Exception: Input is null. Exiting."
-                exit 255
+                # echo -e "Exception: Input is null. Operation skipped."
+                (exit 255)
             fi
 
             # file not found exception
             if [[ ! -e $str_thisFile ]]; then
-                echo -e "Exception: '$str_thisFile' does not exist. Exiting."
-                exit 255
+                # echo -e "Exception: '$str_thisFile' does not exist. Operation skipped."
+                (exit 255)
             fi
 
             # file not readable exception
             if [[ ! -r $str_thisFile ]]; then
-                echo -e "Exception: '$str_thisFile' is not readable. Exiting."
-                exit 255
+                # echo -e "Exception: '$str_thisFile' is not readable. Operation skipped."
+                (exit 255)
             fi
 
-            # one way of creating a backup file
-            # create only one backup file
+        # work #
+            # parameters #
+            declare -r str_suffix=".old"
+            declare -r str_thisDir=$( dirname $1 )
+            declare -ar arr_thisDir=( $( ls -1v $str_thisDir | grep $str_thisFile | grep $str_suffix | uniq ) )
+
+            # positive non-zero count
+            if [[ "${#arr_thisDir[@]}" -ge 1 ]]; then
+
                 # parameters #
-                # str_oldFile1="$1.old"
+                declare -ir int_maxCount=5
+                str_line=${arr_thisDir[0]}
+                str_line=${str_line%"${str_suffix}"}        # substitution
+                str_line=${str_line##*.}                    # ditto
 
-                # # backup exists
-                # if [[ -e $str_oldFile1 ]]; then
+                # check if string is a valid integer #
+                if [ "${str_line}" -eq "$(( ${str_line} ))" ] 2> /dev/null; then
+                    declare -ir int_firstIndex="${str_line}"
 
-                #     # no changes
-                #     if [[ $str_thisFile -ef $str_oldFile1 ]]; then
-                #         echo -e "'$str_thisFile' has no changes from backup. Exiting."
-                #         exit 0
-                #     fi
-
-                #     # source file is newer than backup
-                #     if [[ $str_thisFile -nt $str_oldFile1 ]]; then
-                #         cp $str_thisFile $str_oldFile1
-
-                #     # source file is older than backup file, save newer backup file as "new" and create new backup as "old"
-                #     else
-
-                #         mv $str_oldFile $str_newFile
-                #         cp $str_thisFile $str_oldFile
-                #         echo -e "WARNING: '$str_thisFile' is older than backup."
-                #     fi
-
-                # # create backup
-                # else
-                #    cp $str_thisFile $str_oldFile1
-                # fi
-
-            # another way of creating a backup file(s)
-            # create up to five backup files
-                # parameters #
-                str_oldFile1="$1.old"
-                str_thisDir1=$( dirname $1 )
-                declare -ar arr1=( $( ls $str_thisDir1 | grep -E $str_oldFile1 | uniq ) )
-
-                # positive non-zero count
-                if [[ ${#arr_thisDir1[@]} -gt 0 ]]; then
-
-                    if [[ ${#arr_thisDir1[@]} -eq 1 ]]; then
-
-                        # no changes
-                        if [[ $str_thisFile -ef ${arr_thisDir1[1]} ]]; then
-                            echo -e "'$str_thisFile' has no changes from backup. Exiting."
-                            exit 0
-                        fi
-
-                        # source file is newer than backup
-                        if [[ $str_thisFile -nt ${arr_thisDir1[1] }]; then
-                            cp $str_thisFile $str_oldFile1
-
-                        # source file is older than backup file, save newer backup file as "new" and create new backup as "old"
-                        else
-
-                            mv ${arr_thisDir1[1]} $str_oldFile1"00"$
-                            cp $str_thisFile ${arr_thisDir1[1]}
-                            echo -e "WARNING: '$str_thisFile' is older than backup."
-                        fi
-
-                    else
-
-                        # limit five backup files
-                        if [[ ${#arr1[@]} -gt 5 ]]; then
-                            declare -ir int_count=5
-
-                        else
-                            declare -ir int_count=${#arr1[@]}
-                        fi
-
-                        # parse array in reverse sort, rename each file with a new three-digit integer
-                        for (( int_i=$int_count ; int_i>1 ; i-- )); do
-
-                            # parameters #
-                            str_file1=${arr1[$int_i]}
-                            # str_file2=${{str_file1}%%*${int_i}}    # one way of returning new value
-                            (( int_i++ ))
-
-                            # suffix is a three digit value
-                            case *"${int_i}"* in
-                                {1..9})
-                                    str1="00${int_i}"
-                                    ;;
-
-                                # {10..99})                          # not necessary due to limit of five backup files
-                                #     str1="0${int_i}"
-                                #     ;;
-
-                                # {100..999})
-                                #     str1="${int_i}"
-                                #     ;;
-
-                                # *)
-                                #     echo -e "THAT'S A HUGE BITCH."
-                                #     exit 255
-                                #     ;;
-                            esac
-
-                            # update file #
-                            # str_file2+=$str1                        # ditto; one way
-                            str_file2="${str_oldFile1}${str1}"        # another way of returning new value
-                            mv ${str_file1} ${str_file2}
-
-                            (( int_i-- ))
-                        done
-
-                        #
-                        cp $str_thisFile $str_oldFile1"001"
-                    fi
-
-                # no backups, create backup
                 else
-                    cp $str_thisFile $str_oldFile1"001"
+                    # echo -e "Exception: '$str_line' is not an integer. Operation skipped."
+                    (exit 255)
                 fi
+
+                # # debug #
+                # echo
+                # echo -e "'$int_firstIndex'"
+
+                # if latest backup is same as original file, exit
+                if [[ $str_thisFile -ef ${arr_thisDir[-1]} ]]; then
+                    # echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
+                    (exit 100)
+                fi
+
+                # before backup, delete all but some number of backup files
+                while [[ ${#arr_thisDir[@]} -ge $int_maxCount ]]; do
+                    if [[ -e ${arr_thisDir[0]} ]]; then
+                        rm ${arr_thisDir[0]}
+                        break
+                    fi
+                done
+
+                # if *first* backup is same as original file, exit
+                if [[ $str_thisFile -ef ${arr_thisDir[0]} ]]; then
+                    # echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
+                    (exit 100)
+                fi
+
+                # new parameters #
+                str_line=${arr_thisDir[-1]%"${str_suffix}"}     # substitution
+                str_line=${str_line##*.}                        # ditto
+
+                # check if string is a valid integer #
+                if [ "${str_line}" -eq "$(( ${str_line} ))" ] 2> /dev/null; then
+                    declare -i int_lastIndex="${str_line}"
+
+                else
+                    # echo -e "Exception: '$str_line' is not an integer. Operation skipped."
+                    (exit 255)
+                fi
+
+                # echo -e "'$int_lastIndex'"      # debug #
+                (( int_lastIndex++ ))           # counter
+                # echo -e "'$int_lastIndex'"      # debug #
+
+                # source file is newer and different than backup, add to backups
+                if [[ $str_thisFile -nt ${arr_thisDir[-1]} && ! ( $str_thisFile -ef ${arr_thisDir[-1]} ) ]]; then
+                    cp $str_thisFile "${str_thisFile}.${int_lastIndex}${str_suffix}"
+                    # echo -e "'$str_thisFile' is newer than backup(s). Operation complete."
+                    (exit 0)
+
+                elif [[ $str_thisFile -ot ${arr_thisDir[-1]} ]]; then
+                    # echo -e "'$str_thisFile' is older than backup(s). Operation skipped."
+                    (exit 100)
+
+                else
+                    # echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
+                    (exit 100)
+                fi
+
+            # no backups, create backup
+            else
+                cp $str_thisFile "${str_thisFile}.0${str_suffix}"
+                # echo -e "Operation complete."
+                (exit 0)
+            fi
+
+        case "$?" in
+            0)
+                echo -e "Complete.";;
+
+            255)
+                echo -e "Failed. Thrown exception.";;
+
+            *)
+                echo -e "Skipped.";;
+
+            # NOTES:
+                # be more specific with exit codes?
+                #   the idea is to have *absolute* exit codes (0 or 255) for pass and fail.
+                #   and to have *relative* exit codes (any num between 0 and 255) for specific errors and exceptions
+                #       then output the string to console here (save space writing the same error, per function, per condition statement)
+                #       nd finally override those at the end of a given function, with exit '255'
+
+            # 100)
+            #     echo -e "Skipped.";;
+
+            # *)
+            #     echo -e "Unknown status. Exit code not described.";;
+        esac
     }
 
-# echo filename
-echo $0
+# scratch #
+    function main {
 
-# echo present working directory
-echo
-pwd
-basename $( pwd )   # echo *lowest* child directory
+        # echo filename
+        echo $0
 
-# parse files in current dir #
-echo
-ls $( pwd )
+        # echo present working directory
+        echo
+        pwd
+        basename $( pwd )   # echo *lowest* child directory
 
-# file test
-str_thisFile="test.txt"
+        # parse files in current dir #
+        echo
+        ls $( pwd )
 
-# check if file does NOT exist
-if [[ ! -e $str_thisFile ]]; then        #
-# if [[ -z $str_thisFile ]]; then            # these two statements are NOT equal
-    touch $str_thisFile
+        # file test
+        str_thisFile="test.txt"
 
-    echo -e "hello\nworld" > $str_thisFile
-fi
+        # check if file does NOT exist
+        if [[ ! -e $str_thisFile ]]; then        #
+        # if [[ -z $str_thisFile ]]; then            # these two statements are NOT equal
+            touch $str_thisFile
 
-# check if file exists
-if [[ -e $str_thisFile ]]; then
-    cat $str_thisFile
-fi
+            echo -e "hello\nworld" > $str_thisFile
+        fi
 
-# check if file is readable #
-echo
-if [[ -e $str_thisFile ]]; then
-    cat $str_thisFile
-fi
+        # check if file exists
+        if [[ -e $str_thisFile ]]; then
+            cat $str_thisFile
+        fi
+
+        # check if file is readable #
+        echo
+        if [[ -e $str_thisFile ]]; then
+            cat $str_thisFile
+        fi
+
+    }
+
+# main #
+    CheckIfUserIsRoot
+
+    # file test
+    str_thisFile="test.txt"
+
+    # check if file does NOT exist
+    if [[ ! -e $str_thisFile ]]; then        #
+    # if [[ -z $str_thisFile ]]; then            # these two statements are NOT equal
+        touch $str_thisFile
+    fi
+
+    # echo -e "hello\nworld" > $str_thisFile
+    # echo -e "shalom\nworld" > $str_thisFile
+    echo -e "save the\nworld" > $str_thisFile
+    #echo -e "screw the\nworld" > $str_thisFile
+
+    CreateBackupFromFile $str_thisFile
+
+    echo "'$?'" # exit code from last function
+
+
+
+
 
 
 
