bfd87b0045d93c395cdf1c930bf4fbdd03983acc
portellam@gitkraken <name@example.com>
2022-09-30 22:45:30 -0400

    Update test.bash

--- test.bash
+++ test.bash
@@ -26,139 +26,135 @@
         #
 
         echo -en "Backing up file... "
-        echo -e "$?"
 
         # parameters #
         str_thisFile=$1
 
         # create false match statements before work begins, to catch exceptions
-            # null exception
-            if [[ -z $str_thisFile ]]; then
-                # echo -e "Exception: Input is null. Operation skipped."
-                (exit 255)
-            fi
+        # null exception
+        if [[ -z $str_thisFile ]]; then
+            # echo -e "Exception: Input is null. Operation skipped."
+            (exit 255)
+        fi
 
-            # file not found exception
-            if [[ ! -e $str_thisFile ]]; then
-                # echo -e "Exception: '$str_thisFile' does not exist. Operation skipped."
-                (exit 255)
-            fi
+        # file not found exception
+        if [[ ! -e $str_thisFile ]]; then
+            # echo -e "Exception: '$str_thisFile' does not exist. Operation skipped."
+            (exit 255)
+        fi
 
-            # file not readable exception
-            if [[ ! -r $str_thisFile ]]; then
-                # echo -e "Exception: '$str_thisFile' is not readable. Operation skipped."
-                (exit 255)
-            fi
+        # file not readable exception
+        if [[ ! -r $str_thisFile ]]; then
+            # echo -e "Exception: '$str_thisFile' is not readable. Operation skipped."
+            (exit 255)
+        fi
 
         # work #
+        # parameters #
+        declare -r str_suffix=".old"
+        declare -r str_thisDir=$( dirname $1 )
+        declare -ar arr_thisDir=( $( ls -1v $str_thisDir | grep $str_thisFile | grep $str_suffix | uniq ) )
+
+        # positive non-zero count
+        if [[ "${#arr_thisDir[@]}" -ge 1 ]]; then
+
             # parameters #
-            declare -r str_suffix=".old"
-            declare -r str_thisDir=$( dirname $1 )
-            declare -ar arr_thisDir=( $( ls -1v $str_thisDir | grep $str_thisFile | grep $str_suffix | uniq ) )
+            declare -ir int_maxCount=5
+            str_line=${arr_thisDir[0]}
+            str_line=${str_line%"${str_suffix}"}        # substitution
+            str_line=${str_line##*.}                    # ditto
 
-            # positive non-zero count
-            if [[ "${#arr_thisDir[@]}" -ge 1 ]]; then
+            # check if string is a valid integer #
+            if [[ "${str_line}" -eq "$(( ${str_line} ))" ]] 2> /dev/null; then
+                declare -ir int_firstIndex="${str_line}"
 
-                # parameters #
-                declare -ir int_maxCount=5
-                str_line=${arr_thisDir[0]}
-                str_line=${str_line%"${str_suffix}"}        # substitution
-                str_line=${str_line##*.}                    # ditto
-
-                # check if string is a valid integer #
-                if [[ "${str_line}" -eq "$(( ${str_line} ))" ]] 2> /dev/null; then
-                    declare -ir int_firstIndex="${str_line}"
-
-                else
-                    # echo -e "Exception: '$str_line' is not an integer. Operation skipped."
-                    (exit 255)
-                fi
-
-                # # debug #
-                # echo
-                # echo -e "'$int_firstIndex'"
-
-                for str_element in ${arr_thisDir[@]}; do
-                    if cmp -s $str_thisFile $str_element; then
-                        echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
-                        (exit 100)
-                        break
-                    fi
-                done
-
-                # if latest backup is same as original file, exit
-                if cmp -s $str_thisFile ${arr_thisDir[-1]}; then
-                    echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
-                    (exit 100)
-                fi
-
-                # before backup, delete all but some number of backup files
-                while [[ ${#arr_thisDir[@]} -ge $int_maxCount ]]; do
-                    if [[ -e ${arr_thisDir[0]} ]]; then
-                        rm ${arr_thisDir[0]}
-                        break
-                    fi
-                done
-
-                # if *first* backup is same as original file, exit
-                if cmp -s $str_thisFile ${arr_thisDir[0]}; then
-                    # echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
-                    (exit 100)
-                fi
-
-                # new parameters #
-                str_line=${arr_thisDir[-1]%"${str_suffix}"}     # substitution
-                str_line=${str_line##*.}                        # ditto
-
-                # check if string is a valid integer #
-                if [[ "${str_line}" -eq "$(( ${str_line} ))" ]] 2> /dev/null; then
-                    declare -i int_lastIndex="${str_line}"
-
-                else
-                    # echo -e "Exception: '$str_line' is not an integer. Operation skipped."
-                    (exit 255)
-                fi
-
-                # echo -e "'$int_lastIndex'"      # debug #
-                (( int_lastIndex++ ))           # counter
-                # echo -e "'$int_lastIndex'"      # debug #
-
-                # source file is newer and different than backup, add to backups
-                if [[ $str_thisFile -nt ${arr_thisDir[-1]} && ! ( $str_thisFile -ef ${arr_thisDir[-1]} ) ]]; then
-                    cp $str_thisFile "${str_thisFile}.${int_lastIndex}${str_suffix}"
-                    # echo -e "'$str_thisFile' is newer than backup(s). Operation complete."
-                    (exit 0)
-
-                elif [[ $str_thisFile -ot ${arr_thisDir[-1]} && ! ( $str_thisFile -ef ${arr_thisDir[-1]} ) ]]; then
-                    # echo -e "'$str_thisFile' is older than backup(s). Operation skipped."
-                    (exit 100)
-
-                else
-                    # echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
-                    (exit 100)
-                fi
-
-            # no backups, create backup
             else
-                cp $str_thisFile "${str_thisFile}.0${str_suffix}"
-                # echo -e "Operation complete."
-                (exit 0)
+                # echo -e "Exception: '$str_line' is not an integer. Operation skipped."
+                (exit 255)
             fi
+
+            # # debug #
+            # echo
+            # echo -e "'$int_firstIndex'"
+
+            for str_element in ${arr_thisDir[@]}; do
+                if cmp -s $str_thisFile $str_element; then
+                    # echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
+                    (exit 100)
+                    break
+                fi
+            done
+
+            # if latest backup is same as original file, exit
+            if cmp -s $str_thisFile ${arr_thisDir[-1]}; then
+                # echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
+                (exit 100)
+            fi
+
+            # before backup, delete all but some number of backup files
+            while [[ ${#arr_thisDir[@]} -ge $int_maxCount ]]; do
+                if [[ -e ${arr_thisDir[0]} ]]; then
+                    rm ${arr_thisDir[0]}
+                    break
+                fi
+            done
+
+            # if *first* backup is same as original file, exit
+            if cmp -s $str_thisFile ${arr_thisDir[0]}; then
+                # echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
+                (exit 100)
+            fi
+
+            # new parameters #
+            str_line=${arr_thisDir[-1]%"${str_suffix}"}     # substitution
+            str_line=${str_line##*.}                        # ditto
+
+            # check if string is a valid integer #
+            if [[ "${str_line}" -eq "$(( ${str_line} ))" ]] 2> /dev/null; then
+                declare -i int_lastIndex="${str_line}"
+
+            else
+                # echo -e "Exception: '$str_line' is not an integer. Operation skipped."
+                (exit 255)
+            fi
+
+            # echo -e "'$int_lastIndex'"      # debug #
+            (( int_lastIndex++ ))           # counter
+            # echo -e "'$int_lastIndex'"      # debug #
+
+            # source file is newer and different than backup, add to backups
+            if [[ $str_thisFile -nt ${arr_thisDir[-1]} && ! ( $str_thisFile -ef ${arr_thisDir[-1]} ) ]]; then
+                cp $str_thisFile "${str_thisFile}.${int_lastIndex}${str_suffix}"
+                # echo -e "'$str_thisFile' is newer than backup(s). Operation complete."
+                (exit 0)
+
+            elif [[ $str_thisFile -ot ${arr_thisDir[-1]} && ! ( $str_thisFile -ef ${arr_thisDir[-1]} ) ]]; then
+                # echo -e "'$str_thisFile' is older than backup(s). Operation skipped."
+                (exit 100)
+
+            else
+                # echo -e "'$str_thisFile' is same as backup(s). Operation skipped."
+                (exit 100)
+            fi
+
+        # no backups, create backup
+        else
+            cp $str_thisFile "${str_thisFile}.0${str_suffix}"
+            # echo -e "Operation complete."
+            (exit 0)
+        fi
 
         # append output and return code
         # when declaring this function, note the return code and create a condition given if "failure" (failure or just skipping) is mission critical.
             case "$?" in
                 0)
-                    echo -e "Complete."
-                    return 0;;
+                    echo -e "Complete.";;
 
                 255)
-                    echo -e "Failed. Thrown exception."
-                    return 1;;
+                    echo -e "Failed. Thrown exception.";;
 
                 *)
-                    echo -e "Skipped."
-                    return 1;;
+                    echo -e "Skipped.";;
 
                 # NOTES:
                     # be more specific with exit codes?
@@ -198,7 +194,7 @@
         # if [[ -z $str_thisFile ]]; then            # these two statements are NOT equal
             touch $str_thisFile
 
-            echo -e "hello\nworld" > $str_thisFile
+            #echo -e "hello\nworld" > $str_thisFile
         fi
 
         # check if file exists
@@ -231,11 +227,13 @@
     # echo -e "save the\nworld" > $str_thisFile
     # echo -e "screw the\nworld" > $str_thisFile
 
-    if CreateBackupFromFile $str_thisFile; then
-        echo -e "Pass."
+    CreateBackupFromFile $str_thisFile
 
-    else
-        echo -e "Fail."
-    fi
+    # if CreateBackupFromFile $str_thisFile; then
+    #     echo -e "Pass."
 
-    echo "'$?'" # exit code from last function
+    # else
+    #     echo -e "Fail."
+    # fi
+
+    # echo "'$?'" # exit code from last function
